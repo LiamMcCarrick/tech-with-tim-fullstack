4e0c06794f54865022cca2e7ec14dbc0d17f1ae0
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2024-06-07 14:12:26 -0400

    create fetchContacts function to fetch once loaded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  /*Set up state to store contacts*/
+  const [contacts,setContacts] = useState([])
+
+  /*Render once when function is called*/
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  /*Function to send get request to backend to get contacts*/
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    /*Get JSON data associated with request*/
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
 
   return (
     <>
