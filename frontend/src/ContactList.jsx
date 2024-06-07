1eef94a9d91e12cd038f4f1385f9b192ad33963f
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2024-06-07 14:27:46 -0400

    create table that displays contacts

---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,34 @@
+/*Write component to render our contacts*/
+import React from "react"
+
+const ContactList = ({contacts}) => {
+    return <div>
+        <h2>Contacts</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => (
+                    <tr> key={contact.id}
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                        <td>
+                            <button>Update</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+}
+
+/*Export in order to imoprt */
+export default ContactList
