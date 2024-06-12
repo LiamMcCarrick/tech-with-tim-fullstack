b671006da4ee991481102c98c2269d8d64d2c111
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2024-06-11 21:25:35 -0400

    create form modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import ContactForm from './ContactForm'
 function App() {
   /*Set up state to store contacts*/
   const [contacts, setContacts] = useState([])
+  // set modal (detach form) to start as false
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   /*Render once function is called*/
   useEffect(() => {
@@ -21,11 +23,28 @@ function App() {
     console.log(data.contacts)
   }
 
+  // toggle modal
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  // open modal when creating contact
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
   // create fragment to render contact form
   return (
     <>
       <ContactList contacts={contacts} />
-      <ContactForm />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content"></div>
+        <span className="close" onClick={closeModal}>&times;</span>
+        <ContactForm />
+      </div>
+
+      }
     </>
   )
 }
